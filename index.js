b9786757060038253973f8d6c16b196fa5ed72e2
mohnaofal <moh.naofal@gmail.com>
2019-08-09 11:11:52 +0700

    day5

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const array_lib = require("./lib/array")
 const object_lib = require("./lib/object")
 const tugas_lib = require("./lib/tugas")
 const tambah_lib = require("./lib/tambah")
+const conditional = require("./lib/conditional")
+const tugas_if = require("./lib/tugasif")
+const task_condition = require("./lib/task-conditional")
+
 
 app.use(express.urlencoded({extended: true}))
 
@@ -107,6 +111,74 @@ app.get("/tugas2", (req, res) => {
     console.log("Sesudah di tambah ", datamobil)
 
     return res.send(datamobil)
+})
+
+
+app.get("/task3", (req, res) => {
+    let q = req.query.q
+    console.log("First value ", q)
+
+    let result = conditional.equal(q)
+
+    return res.send(result)
+})
+
+app.get("/task4", (req, res) => {
+    let today = new Date().getDay()
+    console.log("Today ", today)
+
+    let result = conditional.day(today)
+    console.log("Hari ini adalah hari "+ result)    
+    return res.send(result)
+})
+
+app.post("/task5", (req, res) => {
+    let number = req.body.number
+    console.log("section1 ", typeof number)
+    number = parseInt(number)
+    console.log("section2 ", typeof number)
+
+    let result = conditional.compare(number)
+
+    return res.send(result)
+})
+
+app.get("/tugasday4", (req, res) => {
+    let q = req.query.q
+    console.log("Kata yang kamu masukkan ", q)
+
+    let result = tugas_if(q)
+
+    return res.send(result)
+})
+
+app.post('/tugas2day4',(req, res) =>{
+    let nama = req.body.nama
+    let email = req.body.email
+
+    result = tugas_if.coba2(nama, email)
+
+    return res.send(`${result}`)
+})
+
+
+app.get("/task1", (req, res) => {
+    let key1 = req.query.key1
+    let key2 = req.query.key2
+
+    let result = task_condition.task1(key1, key2)
+
+    return res.send(result)
+})
+
+app.post("/task2", (req, res) => {
+    let { name, email } = req.body
+    let no = parseInt(req.body.no)
+    let result = task_condition.task2(name, email, no)
+
+    console.log(Object.keys(result))
+
+    return res.send(result)
 })
 
 
